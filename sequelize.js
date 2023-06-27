e191f11d9557301a8103505bdeec0c73e1c9b2d7
asfak06 <asfakmunna@gmail.com>
2023-06-28 01:15:43 +0600

    2nd commit

--- sequelize.js
+++ sequelize.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('sql12629145', 'sql12629145', 'itWEFvfXkR', {
   host: 'sql12.freemysqlhosting.net',
   dialect: 'mysql',
+  dialectModule: require('mysql2')
 });
 
 module.exports = sequelize;
